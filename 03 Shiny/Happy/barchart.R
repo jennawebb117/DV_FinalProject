db4d5fb90a351a68ea5546de659e6559da7ebbef
Happy Situ <happysitu@utexas.edu>
2015-12-06 15:59:31 -0600

    yo

--- 03 Shiny/Happy/barchart.R
+++ 03 Shiny/Happy/barchart.R
@@ -3,30 +3,61 @@ require("RCurl")
 require(ggplot2)
 require(dplyr)
 
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          
-"select AGE_OF_CASUALTY, SUM(NUMBER_OF_CASUALTIES) as some
-from LEEDS_ROAD_ACCIDENTS_2013_2014
-                                                      ;"
-                                                      ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) 
-View(df)
-
-dfFemale <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          
-"select AGE_OF_CASUALTY, CASUALTY_CLASS, SUM_NUMBER_OF_CASUALTIES, sum(SUM_NUMBER_OF_CASUALTIES)  
-OVER(PARTITION BY CASUALTY_CLASS) as window_avg_count
+dfALLSEX <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          
+"select AGE_OF_CASUALTY, SUM_NUMBER_OF_CASUALTIES, sum(SUM_NUMBER_OF_CASUALTIES)
+OVER() as TOTAL_CASUALTIES
 from 
-(select AGE_OF_CASUALTY, CASUALTY_CLASS, sum(NUMBER_OF_CASUALTIES) as SUM_NUMBER_OF_CASUALTIES
+(select AGE_OF_CASUALTY, sum(NUMBER_OF_CASUALTIES) as SUM_NUMBER_OF_CASUALTIES
 from LEEDS_ROAD_ACCIDENTS_2013_2014
-where ( CASUALTY_CLASS=(\'Driver or rider\') and SEX_OF_CASUALTY=(\'Male\') OR SEX_OF_CASUALTY=(\'Female\') )
-group by AGE_OF_CASUALTY, CASUALTY_CLASS)
-order by CASUALTY_CLASS;"
-')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+where (SEX_OF_CASUALTY=(\'Male\') OR SEX_OF_CASUALTY=(\'Female\') )
+group by AGE_OF_CASUALTY)
+order by AGE_OF_CASUALTY desc;"
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) 
 
-dfchl <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          
-"select YEAR, DISEASE, sum_COUNT, sum(sum_COUNT)                                OVER (PARTITION BY DISEASE) as window_avg_COUNT
+
+dfMALE <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          
+"select AGE_OF_CASUALTY, SUM_NUMBER_OF_CASUALTIES, sum(SUM_NUMBER_OF_CASUALTIES)
+OVER() TOTAL_CASUALTIES
 from 
-(select YEAR, DISEASE, sum(COUNT) as sum_COUNT
-from Infectious_Diseases
-where DISEASE = (\'Chlamydia\') and (SEX=(\'Male\') OR SEX=(\'Female\') )
-group by YEAR, DISEASE)
-order by DISEASE;"
-')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+(select AGE_OF_CASUALTY, sum(NUMBER_OF_CASUALTIES) as SUM_NUMBER_OF_CASUALTIES
+from LEEDS_ROAD_ACCIDENTS_2013_2014
+where (SEX_OF_CASUALTY=(\'Male\'))
+group by AGE_OF_CASUALTY)
+order by AGE_OF_CASUALTY desc;"
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) 
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='NUMBER OF CASUALTIES BY AGE') +
+  labs(x=paste("AGE"), y=paste("NUMBER OF CASUALTIES")) +
+  layer(data=dfALLSEX, 
+        mapping=aes(x=AGE_OF_CASUALTY, y=SUM_NUMBER_OF_CASUALTIES), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="bar",
+        geom_params=list(fill="RED"), 
+        position=position_identity()
+  ) + coord_flip() +
+  layer(data=dfMALE, 
+        mapping=aes(x=AGE_OF_CASUALTY, y=SUM_NUMBER_OF_CASUALTIES), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="bar",
+        geom_params=list(fill="#339933"), 
+        position=position_identity()
+  ) + coord_flip() +
+  layer(data=df, 
+        mapping=aes(yintercept = 8829/95), 
+        geom="hline",
+        geom_params=list(colour="BLACK")
+  ) +
+  layer(data=df, 
+        mapping=aes(x=0, y=round(8829/95), label=round(8829/95)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-.2), 
+        position=position_identity()
+  )
--- 03 Shiny/Happy/server.R
+++ 03 Shiny/Happy/server.R
@@ -6,172 +6,69 @@ require(dplyr)
 require(shiny)
 require(shinydashboard)
 require(leaflet)
-require(DT)
 
 shinyServer(function(input, output) {
   
-  KPI_Low_Max_value <- reactive({input$KPI1})     
-  KPI_Medium_Max_value <- reactive({input$KPI2})
-  rv <- reactiveValues(alpha = 0.50)
-  observeEvent(input$light, { rv$alpha <- 0.50 })
-  observeEvent(input$dark, { rv$alpha <- 0.75 })
-  
-  df1 <- eventReactive(input$clicks1, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-                                                                                 "select color, clarity, sum_price, round(sum_carat) as sum_carat, kpi as ratio, 
-                                                                                 case
-                                                                                 when kpi < "p1" then \\\'03 Low\\\'
-                                                                                 when kpi < "p2" then \\\'02 Medium\\\'
-                                                                                 else \\\'01 High\\\'
-                                                                                 end kpi
-                                                                                 from (select color, clarity, 
-                                                                                 sum(price) as sum_price, sum(carat) as sum_carat, 
-                                                                                 sum(price) / sum(carat) as kpi
-                                                                                 from diamonds
-                                                                                 group by color, clarity)
-                                                                                 order by clarity;"
-                                                                                 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', 
-                                                                                                   MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value(), p2=KPI_Medium_Max_value()), verbose = TRUE)))
-  })
-  
-  output$distPlot1 <- renderPlot({             
-    plot1 <- ggplot() + 
-      coord_cartesian() + 
-      scale_x_discrete() +
-      scale_y_discrete() +
-      labs(title=isolate(input$title)) +
-      labs(x=paste("COLOR"), y=paste("CLARITY")) +
-      layer(data=df1(), 
-            mapping=aes(x=COLOR, y=CLARITY, label=SUM_PRICE), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="text",
-            geom_params=list(colour="black"), 
-            position=position_identity()
-      ) +
-      layer(data=df1(), 
-            mapping=aes(x=COLOR, y=CLARITY, fill=KPI), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="tile",
-            geom_params=list(alpha=rv$alpha), 
-            position=position_identity()
-      )
-    plot1
-  }) 
-  
-  observeEvent(input$clicks, {
-    print(as.numeric(input$clicks))
-  })
-  
-  # Begin code for Second Tab:
-  
-  dfchl <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          "select YEAR, DISEASE, sum_COUNT, sum(sum_COUNT)                                OVER (PARTITION BY DISEASE) as window_avg_COUNT
-                                                     from 
-                                                     (select YEAR, DISEASE, sum(COUNT) as sum_COUNT
-                                                     from Infectious_Diseases
-                                                     where DISEASE = (\'Chlamydia\') and (SEX=(\'Male\') OR SEX=(\'Female\') )
-                                                     group by YEAR, DISEASE)
-                                                     order by DISEASE;"
-                                                     ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-  
-  
-  dfMalechl <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-                                                         "select YEAR, DISEASE, sum_COUNT, sum(sum_COUNT) 
-                                                         OVER (PARTITION BY DISEASE) as window_avg_COUNT
-                                                         from 
-                                                         (select YEAR, DISEASE, sum(COUNT) as sum_COUNT
-                                                         from Infectious_Diseases
-                                                         where DISEASE = (\'Chlamydia\') and SEX=(\'Male\')
-                                                         group by YEAR, DISEASE)
-                                                         order by DISEASE;"
-                                                         ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-  
-  
-  output$distPlotchl <- renderPlot(height=300, width=700, {
-    plotchl <- ggplot() + 
+ALLSEX <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=                                          
+"select AGE_OF_CASUALTY, SUM_NUMBER_OF_CASUALTIES, sum(SUM_NUMBER_OF_CASUALTIES)
+OVER() as TOTAL_CASUALTIES
+from 
+(select AGE_OF_CASUALTY, sum(NUMBER_OF_CASUALTIES) as SUM_NUMBER_OF_CASUALTIES
+from LEEDS_ROAD_ACCIDENTS_2013_2014
+where (SEX_OF_CASUALTY=(\'Male\') OR SEX_OF_CASUALTY=(\'Female\') )
+group by AGE_OF_CASUALTY)
+order by AGE_OF_CASUALTY desc;"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) 
+
+MALE <- eventReactive(input$clicks_MALE, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"select AGE_OF_CASUALTY, SUM_NUMBER_OF_CASUALTIES, sum(SUM_NUMBER_OF_CASUALTIES)
+OVER() TOTAL_CASUALTIES                                                         from 
+(select AGE_OF_CASUALTY, sum(NUMBER_OF_CASUALTIES) as SUM_NUMBER_OF_CASUALTIES
+from LEEDS_ROAD_ACCIDENTS_2013_2014
+where (SEX_OF_CASUALTY=(\'Male\'))
+group by AGE_OF_CASUALTY)
+order by AGE_OF_CASUALTY desc;"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+})
+
+
+  output$distPlot_happy <- renderPlot({             
+    plot_happy <- ggplot() + 
       coord_cartesian() + 
       scale_x_continuous() +
       scale_y_continuous() +
-      labs(title='CHLAMYDIA AVERAGE_COUNT, WINDOW_AVG_COUNT') +
-      labs(x=paste("YEAR"), y=paste("COUNT OVER ALL COUNTIES PER YEAR")) +
-      layer(data=dfchl, 
-            mapping=aes(x=YEAR, y=SUM_COUNT), 
+      labs(title='NUMBER OF CASUALTIES BY AGE') +
+      labs(x=paste("AGE"), y=paste("NUMBER OF CASUALTIES")) +
+      layer(data=ALLSEX, 
+            mapping=aes(x=AGE_OF_CASUALTY, y=SUM_NUMBER_OF_CASUALTIES), 
             stat="identity", 
             stat_params=list(), 
             geom="bar",
             geom_params=list(fill="RED"), 
             position=position_identity()
       ) + coord_flip() +
-      layer(data=dfMalechl, 
-            mapping=aes(x=YEAR, y=SUM_COUNT), 
+      layer(data=MALE(), 
+            mapping=aes(x=AGE_OF_CASUALTY, y=SUM_NUMBER_OF_CASUALTIES), 
             stat="identity", 
             stat_params=list(), 
             geom="bar",
-            geom_params=list(fill="BLUE"), 
+            geom_params=list(fill="#339933"), 
             position=position_identity()
       ) + coord_flip() +
-      layer(data=dfchl, 
-            mapping=aes(yintercept = WINDOW_AVG_COUNT/14), 
+      layer(data=ALLSEX, 
+            mapping=aes(yintercept = 8829/95), 
             geom="hline",
             geom_params=list(colour="BLACK")
       ) +
-      layer(data=dfchl, 
-            mapping=aes(x=2000, y=round(WINDOW_AVG_COUNT/14), label=round(WINDOW_AVG_COUNT/14)), 
+      layer(data=ALLSEX, 
+            mapping=aes(x=0, y=round(8829/95), label=round(8829/95)), 
             stat="identity", 
             stat_params=list(), 
             geom="text",
-            geom_params=list(colour="black", hjust=-.1), 
+            geom_params=list(colour="black", hjust=-.2), 
             position=position_identity()
-      )
-    plotchl
-  })
+      ) 
+    plot_happy
+  }) 
   
-  # Begin code for Third Tab:
   
-  df3 <- eventReactive(input$clicks3, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-                                                                                 """select region || \\\' \\\' || \\\'Sales\\\' as measure_names, sum(sales) as measure_values from SUPERSTORE_SALES_ORDERS
-                                                                                 where country_region = \\\'United States of America\\\'
-                                                                                 group by region
-                                                                                 union all
-                                                                                 select market || \\\' \\\' || \\\'Coffee_Sales\\\' as measure_names, sum(coffee_sales) as measure_values from COFFEE_CHAIN
-                                                                                 group by market
-                                                                                 order by 1;"""
-                                                                                 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', 
-                                                                                                   MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
   })
-  
-  output$distPlot3 <- renderPlot(height=1000, width=2000, {
-    plot3 <- ggplot() + 
-      coord_cartesian() + 
-      scale_x_discrete() +
-      scale_y_continuous() +
-      #facet_wrap(~CLARITY, ncol=1) +
-      labs(title='Blending 2 Data Sources') +
-      labs(x=paste("Region Sales"), y=paste("Sum of Sales")) +
-      layer(data=df3(), 
-            mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="bar",
-            geom_params=list(colour="blue"), 
-            position=position_identity()
-      ) + coord_flip() +
-      layer(data=df3(), 
-            mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES, label=round(MEASURE_VALUES)), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="text",
-            geom_params=list(colour="black", hjust=-0.5), 
-            position=position_identity()
-      )
-    plot3
-  })
-  
-  # Begin code for Fourth Tab:
-  output$map <- renderLeaflet({leaflet() %>% addTiles() %>% setView(-93.65, 42.0285, zoom = 17) %>% addPopups(-93.65, 42.0285, 'Here is the Department of Statistics, ISU')
-  })
-  
-  # Begin code for Fifth Tab:
-  output$table <- renderDataTable({datatable(df1())
-  })
-})
+
