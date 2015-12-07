08b881937e0fbb4d2181b9c42642edda358754a4
jennawebb117 <jennawebb@utexas.edu>
2015-12-06 20:40:37 -0600

    Updated html, and added dataframe.R

---
+++ 01 Data/dataframe.R
@@ -0,0 +1,7 @@
+require("jsonlite")
+require("RCurl")
+require(ggplot2)
+require(dplyr)
+
+accidents <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"select * from Leeds_road_accidents_2013_2014;"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_jnw653', PASS='orcl_jnw653', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
